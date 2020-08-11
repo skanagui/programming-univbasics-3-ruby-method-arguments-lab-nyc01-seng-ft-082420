11414d0afa0f8dd45cab3e7291adc707e25f9248
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-11 22:54:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,11 +7,11 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hello, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name,language = "Ruby")
-  puts "Hello, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 
